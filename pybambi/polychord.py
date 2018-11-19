f2197a1fd888ea1a5669a50fec7cefbe1e884473
Will Handley <wh260@cam.ac.uk>
2018-11-19 21:09:16 +0000

    polychord now makes directories

--- pybambi/polychord.py
+++ pybambi/polychord.py
@@ -48,6 +48,14 @@ def run_polychord(loglikelihood, prior, dumper, nDims, nlive, root,
     import pypolychord
     from pypolychord.settings import PolyChordSettings
 
+    basedir = os.path.dirname(root)
+    cluster_dir = os.path.join(basedir,'clusters')
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+
+    if not os.path.exists(cluster_dir):
+        os.makedirs(cluster_dir)
+
     nDerived = 0
     settings = PolyChordSettings(nDims, nDerived)
     settings.base_dir = os.path.dirname(root)
