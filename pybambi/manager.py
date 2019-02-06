39f73a45d5f10fe064b171efba42780acfa29a0b
Pat Scott <p.scott@imperial.ac.uk>
2019-02-06 18:07:25 +0000

    Added test of network failure 	modified:   pybambi/manager.py

--- pybambi/manager.py
+++ pybambi/manager.py
@@ -23,10 +23,12 @@ class BambiManager(object):
 
     old_learners = []
 
+
     def __init__(self, loglikelihood, learner):
         self._loglikelihood = loglikelihood
         self._learner = learner
         self._proxy_trained = False
+        self._rolling_failure_fraction = 0.0
 
     def make_learner(self, params, loglikes):
         if self._learner == 'keras':
@@ -36,24 +38,34 @@ class BambiManager(object):
         else:
             raise NotImplementedError('learner %s is not implemented.' % learner)
 
+
     def dumper(self, live_params, live_loglikes, dead_params, dead_loglikes):
         print("-----------------------------")
         print("Use thumper to do stuff here")
         print("live_params is an array of shape ", live_params.shape)
         print("dead_params is an array of shape ", dead_params.shape)
         print("-----------------------------")
-        if not self._proxy_trained and #hit updint/2:
+        if not self._proxy_trained: #and reached updint/2:
             train_new_learner(np.concatenate(live_params, dead_params), np.concatenate(live_loglikes, dead_loglikes))
 
 
-    def get_loglikelihood(self, theta):
-        # Do some kind of logic to determine if proxy is good enough
-        good_enough = False
-        if self._proxy_trained and good_enough:
-            # Use proxy
-            pass
+    def loglikelihood(self, params):
+
+        # Short circuit to the full likelihood if the proxy is not yet fully trained
+        if not self._proxy_trained: return self._loglikelihood(params)
+
+        # Call the learner and check whether its estimate is to be trusted
+        candidate_loglikelihood = self._current_learner(params)
+        good_enough = logLInRangeOfTrainingData(candidate_likelihood)
+
+        # If the learner can be trusted, use its estimate, otherwise use the original like and update the failure status
+        if good_enough:
+            return candidate_loglikelihood
         else:
-            return self._loglikelihood(theta)
+            self._rolling_failure_fraction = (1.0 + (ntrain - 1.0)*self._rolling_failure_fraction)/ntrain
+            if self._rolling_failure_fraction > self._failure_tolerance: self._proxy_trained = False
+            return self._loglikelihood(params)
+
 
     def train_new_learner(self, params, loglikes):
         self.old_learners.append(self.current_learner)
@@ -62,3 +74,4 @@ class BambiManager(object):
 
 
     def retrain_old_learner(self, learner):
+        pass
